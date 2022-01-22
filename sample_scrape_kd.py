48643a1870c05d3f6705d2dd769ecfbe030d1b28
nozwock <57829219+nozwock@users.noreply.github.com>
2022-01-22 23:51:05 +0530

    feat: Added a sample sample_scrape_kd.py
    - Added requirements-dev.txt

---
+++ sample_scrape_kd.py
@@ -0,0 +1,52 @@
+import re
+from pathlib import Path
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+from article_ripper import get_document, html_to_md
+
+START_CH = 1
+LAST_CH = 246
+LINK_KD = "https://rainingtl.org/kidnapped-dragons-"
+DEST_DIR = Path.cwd().joinpath("dst")
+
+# Scraping with some post-processing stuff.
+# Keep in mind that the steps during the post-processing stuff
+# is obviously very specific to this novel/format of article.
+
+ep_history = []
+head_not_found_err = []
+
+for i in tqdm(range(START_CH, LAST_CH + 1), "processing...🐧️"):
+    doc_html = get_document(LINK_KD + f"{i}").summary()
+    soup = bs(doc_html, "lxml")
+    # Removal of "next/prev chapter" elements.
+    for j in soup.find_all("p")[-10:]:
+        if re.search(r"(previous|next)\schapter", j.text, flags=re.IGNORECASE):
+            j.extract()
+    # h3 to h2
+    if soup.find("h3") is None:
+        head_not_found_err.append(i)
+        try:
+            ep_num = ep_history[-1]
+        except Exception:
+            ep_num = "err"
+    else:
+        soup.find("h3").name = "h2"
+        heading = soup.find("h2").text
+        ep_num = re.findall(r"\d+", heading)[0]
+        if ep_num not in ep_history:
+            # add_break = soup.new_tag("h1")
+            # add_break.string = re.findall(r"[\w ]+:[\w ]+", heading)[0].strip()
+            # soup.h2.insert_before(add_break)
+            soup.find("h2").name = "h1"
+            ep_history.append(ep_num)
+
+    doc_md = html_to_md(soup)
+    ep_dir = DEST_DIR.joinpath(f"{ep_num}-episode")
+    ep_dir.mkdir(parents=True, exist_ok=True)
+
+    with open(ep_dir.joinpath(f"{ep_num}-ch{i}.md"), "w") as f:
+        f.write(doc_md)
+
+if head_not_found_err:
+    print(f"\033[0;1mNo heading found for chapters {head_not_found_err}💔️\033[0m")
