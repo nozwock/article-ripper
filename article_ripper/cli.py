6979ed312e1bcbcf5c66a5a2e1160f541668df9b
nozwock <57829219+nozwock@users.noreply.github.com>
2022-01-22 22:58:41 +0530

    feat: Added __main__.py and cli.py for a cli interface

---
+++ article_ripper/cli.py
@@ -0,0 +1,13 @@
+import click
+from article_ripper.main import get_document, html_text_to_md
+
+
+@click.command()
+@click.option("--url")
+@click.option("--out", default="out.md")
+def cli(url, out):
+    click.echo("Hey ✨")
+
+
+if __name__ == "__main__":
+    cli()
