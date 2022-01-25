7ba5a9031b50e70fc66689de16ae8a4857f64958
nozwock <57829219+nozwock@users.noreply.github.com>
2022-01-25 17:49:38 +0530

    refactor(scrape_kd): Switched to re.findall() wherever could since it felt better for the use case.

--- sample/scrape_kd.py
+++ sample/scrape_kd.py
@@ -27,10 +27,10 @@ if __name__ == "__main__":
     cached_html_chapters_num = []
     for i in html_dir.rglob("*html"):
         cached_html_chapters_num.append(
-            int(re.search(r"(ch(\d{1,3}))", str(i), flags=re.I).groups()[-1])
+            int(re.findall(r"(ch(\d{1,3}))", str(i), flags=re.I)[-1])
         )
 
-    #! Downloading chapters separately
+    # ! Downloading chapters separately
     for i in tqdm(
         range(START_CHAPTER, LAST_CHAPTER + 1),
         "\033[0;1;91mFetching Chapters...\033[0m🚀️",
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         with open(html_episode_dir.joinpath(f"{episode_number}-ch{i}.html"), "w") as f:
             f.write(doc_html)
 
-    #! Updating cache
+    # ! Updating cache
     for i in html_dir.rglob("*html"):
         cached_html_chapters_path.append(i)
 
@@ -76,31 +76,29 @@ if __name__ == "__main__":
     for i in tqdm(
         cached_html_chapters_path, "\033[0;1;94mProcessing...\033[0m⚙️"
     ):
-        episode_number = re.search(r"((\d{1,3})-episode)", str(i), flags=re.I).groups()[
-            -1
-        ]
+        episode_number = re.findall(r"((\d{1,3})-episode)", str(i), flags=re.I)[-1]
         if len(episode_number) == 1:
             episode_number = "0" + episode_number
-        chapter_number = re.search(r"(ch(\d{1,3}))", str(i), flags=re.I).groups()[-1]
+        chapter_number = re.findall(r"(ch(\d{1,3}))", str(i), flags=re.I)[-1]
         with open(i, "r") as f:
-            #! Removal of "next/prev chapter" elements.
+            # ! Removal of "next/prev chapter" elements.
             soup = bs(f, "lxml")
             for j in soup.find_all("p")[-10:]:
                 if re.search(r"(previous|next)\s+chapter", j.text, flags=re.IGNORECASE):
                     j.extract()
-            #! h3 to h2
+            # ! h3 to h2
             heading = soup.find("h3")
             if heading is None:
                 head_not_found_err.append(f"ep{episode_history[-1]} ch{chapter_number}")
 
                 for i in soup.find_all("strong"):
-                    #! lazy workaround for when can't find hx
+                    # ! lazy workaround for when can't find hx
                     if re.search(r"episode\s+\d{2,3}:", str(i), flags=re.I):
                         i.parent.name = "h2"
             else:
                 heading.name = "h2"
                 if episode_number not in episode_history:
-                    heading.name = "h1"  #! for chapter break when converting to epub
+                    heading.name = "h1"  # ! for chapter break when converting to epub
                     episode_history.append(episode_number)
 
         doc_md = html_to_md(soup)
