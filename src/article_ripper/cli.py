fed019d9fb10cd836bb3f9a5eb9e7565172b010e
nozwock <57829219+nozwock@users.noreply.github.com>
2022-01-26 19:14:03 +0530

    Add a simple cli via typer for now

--- src/article_ripper/cli.py
+++ src/article_ripper/cli.py
@@ -1,13 +1,24 @@
-import click
+import typer
+from typing import Optional
+from article_ripper import get_document, html_to_md
+
+app = typer.Typer()
 
 
-@click.command()
-@click.option("--url")
-@click.option("--out", default="out.md")
-def cli(url, out):
-    click.echo("Hey ✨")
-# ! Pending
+@app.command()
+def fun(url: str, out: Optional[str] = None) -> None:
+    doc = get_document(url)
+    doc_summary = doc.summary()
+    if out is None:
+        print(doc_summary)
+    else:
+        with open(out, 'w') as f:
+            f.write(doc_summary)
+
+
+def run() -> None:
+    app()
 
 
 if __name__ == "__main__":
-    cli()
+    run()
